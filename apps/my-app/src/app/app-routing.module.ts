0819c36b93b063407fede525e3100301ef3bdfe8
adsk0165 <a.charkaoui@student.avans.nl>
2024-11-19 16:10:15 +0100

    10 deploymeny try

--- apps/my-app/src/app/app-routing.module.ts
+++ apps/my-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsComponent, UserEditComponent } from '@avans-nx-workshop/features';
 import { UserListComponent } from '@avans-nx-workshop/features'; 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutComponent } from './components/about/about.component';
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent },
   {path: 'edit/:id', component: UserEditComponent},
   { path: 'users', component: UserListComponent },   
-  { path: 'user/:id', component: UserDetailsComponent },  
-  { path: '', redirectTo: '/users', pathMatch: 'full' }  
+  { path: 'user/:id', component: UserDetailsComponent },
+  { path: 'about', component: AboutComponent },    
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }  
 ];
 
 @NgModule({
