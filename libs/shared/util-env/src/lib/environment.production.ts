e0211eb75c55cf341fc9f8d0e212f94d62b440b4
adsk0165 <a.charkaoui@student.avans.nl>
2024-11-28 14:09:20 +0100

    working

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -6,5 +6,5 @@ export const environment: IEnvironment = {
     ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
     dataApiUrl: 'https://nxworkshop.azurewebsites.net/api',
 
-    MONGO_DB_CONNECTION_STRING: 'mongodb+srv://gamewatch123:<AvansStudent123>@cluster0.ppnjhh5.mongodb.net/indvprojectclientside?retryWrites=true&w=majority&appName=Cluster0'
+    MONGO_DB_CONNECTION_STRING: 'mongodb+srv://gamewatch123:wFg0OB1bINwjrsnJ@cluster0.ppnjhh5.mongodb.net/indvprojectclientside?retryWrites=true&w=majority&appName=Cluster0'
 };
