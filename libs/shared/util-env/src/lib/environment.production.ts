c100ad9bd4f5a1dd4bf52c871a6cd36e0ec0427e
adsk0165 <a.charkaoui@student.avans.nl>
2024-12-03 12:31:30 +0100

    changed url data url

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -4,7 +4,7 @@ export const environment: IEnvironment = {
     production: true,
 
     ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
-    dataApiUrl: 'https://nxworkshop.azurewebsites.net/api',
+    dataApiUrl: 'https://dataappvoorindv-ajaqfmdzdwbbawf4.westeurope-01.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://gamewatch123:wFg0OB1bINwjrsnJ@cluster0.ppnjhh5.mongodb.net/indvprojectclientside?retryWrites=true&w=majority&appName=Cluster0'
 };
