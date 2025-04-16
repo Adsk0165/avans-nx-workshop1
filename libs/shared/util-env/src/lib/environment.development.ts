5ae7e0e0a21f7dbcccdc0f6ae3d95798e0f11e3a
adsk0165 <a.charkaoui@student.avans.nl>
2025-04-16 23:19:21 +0200

    env

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -7,8 +7,8 @@ export const environment: IEnvironment = {
     dataApiUrl: 'http://localhost:3000/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb://localhost:27017/shareameal',
-    RCMND_NEO4J_DB_HOST: '0d7b95a5.databases.neo4j.io',
+    RCMND_NEO4J_DB_HOST: '9c195864.databases.neo4j.io',
     RCMND_NEO4J_DB_PORT: 7687,
     RCMND_NEO4J_DB_USER: 'neo4j',
-    RCMND_NEO4J_DB_PASSWORD: 'NWI9GV2f9a5mHtfFiZFoAuLG--ofDQCddlYoyrn79v4'
+    RCMND_NEO4J_DB_PASSWORD: '_7SjqbEUEcgaQts1CmBhSH-CMheariLfS7wamm3BRWY'
 };
--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -8,8 +8,8 @@ export const environment: IEnvironment = {
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://gamewatch123:wFg0OB1bINwjrsnJ@cluster0.ppnjhh5.mongodb.net/indvprojectclientside?retryWrites=true&w=majority&appName=Cluster0',
 
-    RCMND_NEO4J_DB_HOST: '0d7b95a5.databases.neo4j.io',
+    RCMND_NEO4J_DB_HOST: '9c195864.databases.neo4j.io',
     RCMND_NEO4J_DB_PORT: 7687,
     RCMND_NEO4J_DB_USER: 'neo4j',
-    RCMND_NEO4J_DB_PASSWORD: 'NWI9GV2f9a5mHtfFiZFoAuLG--ofDQCddlYoyrn79v4'
+    RCMND_NEO4J_DB_PASSWORD: '_7SjqbEUEcgaQts1CmBhSH-CMheariLfS7wamm3BRWY'
 };
